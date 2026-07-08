88be7c84bf32697b0ef8ecd8cafc089f049e9022
Pepa <9425683+pepa667@users.noreply.github.com>
2026-07-08 20:03:32 -0300

    Last time fixes

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,28 @@
-import { defineConfig } from "vite";
-import preact from "@preact/preset-vite";
-import webfontDl from "vite-plugin-webfont-dl";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite';
+import preact from '@preact/preset-vite';
+// import webfontDl from "vite-plugin-webfont-dl";
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     preact(),
     tailwindcss(),
-    webfontDl(
-      [
-        "https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700;800&display=swap",
-      ],
-      {
-        // 1. Força o plugin a injetar a tag <link rel="preload"> no HTML final de build automaticamente
-        injectAs: "preload",
+    // webfontDl(
+    //   [
+    //     "https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700;800&display=swap",
+    //   ],
+    //   {
+    //     // 1. Força o plugin a injetar a tag <link rel="preload"> no HTML final de build automaticamente
+    //     injectAs: "preload",
 
-        // 2. Filtra para o Google mandar APENAS os caracteres latinos (PT-BR)
-        // Arranca fora cirílico, grego e símbolos desnecessários, reduzindo o arquivo em até 70%
-        subsets: ["latin"],
+    //     // 2. Filtra para o Google mandar APENAS os caracteres latinos (PT-BR)
+    //     // Arranca fora cirílico, grego e símbolos desnecessários, reduzindo o arquivo em até 70%
+    //     subsets: ["latin"],
 
-        // 3. Garante que o CSS venha com font-display: swap ativo
-        fontDisplay: "swap",
-      },
-    ),
+    //     // 3. Garante que o CSS venha com font-display: swap ativo
+    //     fontDisplay: "swap",
+    //   },
+    // ),
   ],
 });
